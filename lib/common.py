a8d24464b479a112690db3586aeecbd0d49a33d9
bitacademy-dl <bitacademy.dl@gmail.com>
2020-11-18 16:36:17 +0900

    module common MLS 추가

--- lib/common.py
+++ lib/common.py
@@ -17,3 +17,33 @@ def numerical_gradient(f, x):
         x[i] = tmp
 
     return gradient
+
+
+def gradient_descent(f, x, lr=0.01, epoch=100):
+    for i in range(epoch):
+        gradient = numerical_gradient(f, x)
+        # 출력
+        print(f'epoch={i+1}, gradient={gradient}, x={x}')
+        x -= lr * gradient
+
+    return x
+
+
+def method_least_squares(x, y):
+    mx = sum(x)/len(x)
+    my = sum(y)/len(y)
+
+    # s1 = 0
+    # for i in range(len(x)):
+    #     s1 += (x[i] - mx) * (y[i] - my)
+    s1 = sum([(i - mx)*(j - my) for i, j in zip(x, y)])
+
+    # s2 = 0
+    # for i in range(len(x)):
+    #     s2 += (x[i] - mx)**2
+    s2 = sum([(i-mx)**2 for i in x])
+
+    mls_a = s1 / s2
+    mls_b = my - (mx * mls_a)
+
+    return mls_a, mls_b
