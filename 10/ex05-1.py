1d466267c163eb63ee898b5e2a93cf75f93b0876
bitacademy-dl <bitacademy.dl@gmail.com>
2020-11-20 13:31:47 +0900

    다중선형회귀(해석편미분) 연습#4

--- 10/ex05-1.py
+++ 10/ex05-1.py
@@ -2,6 +2,7 @@
 import os
 import sys
 from pathlib import Path
+from matplotlib import pyplot as plt
 try:
     sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
     from common import gradient_descent
@@ -37,3 +38,13 @@ x1_p = 4
 x2_p = 0
 y_p = result[0] * x1_p + result[1] * x2_p + result[2]
 print(f'공부를 {x1_p}시간 하고 과외를 {x2_p}시간 받았을  때, 받을 수 있는 점수는 {y_p}점')
+
+# 그래프
+axes = plt.axes(projection='3d')
+axes.scatter(times, ptimes, scores)
+# axes.plot(times, ptimes, scores)
+axes.set_xlabel('Study Hours')
+axes.set_ylabel('Private Class Hours')
+axes.set_zlabel('Scores')
+
+plt.show()
